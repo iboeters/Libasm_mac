1deef74fabed084ae79e7554ee53187dbe78be12
Iris <iboeters@LAPTOP-MGJ0DHHN.localdomain>
2021-01-19 12:08:17 +0100

    errno

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: iboeters <iboeters@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/01/15 13:37:27 by iboeters      #+#    #+#                 */
-/*   Updated: 2021/01/19 12:07:21 by iboeters      ########   odam.nl         */
+/*   Updated: 2021/01/19 12:08:06 by iboeters      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,7 @@ int		main(void)
 
 		printf("\ntest ft_write\n");
 		fd = open("file.txt", O_RDONLY);
-		printf("real |%ld| |%d|\n", write(fd, s4, len), errno);
+		// printf("real |%ld| |%d|\n", write(fd, s4, len), errno);
 		printf("own  |%ld| |%d|\n", ft_write(fd, s4, len), errno);
 	}
 	{
