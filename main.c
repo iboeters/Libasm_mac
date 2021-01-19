becf6d6d0c807d5708fb949c36ea7308de1e08e9
Iris <iboeters@LAPTOP-MGJ0DHHN.localdomain>
2021-01-19 12:40:05 +0100

    joe

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: iboeters <iboeters@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/01/15 13:37:27 by iboeters      #+#    #+#                 */
-/*   Updated: 2021/01/19 12:38:47 by iboeters      ########   odam.nl         */
+/*   Updated: 2021/01/19 12:39:59 by iboeters      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,7 +52,7 @@ int		main(void)
 		int fd2 = open("filee.txt", O_WRONLY);
 		char *s5 = "yayha";
 		int len2 = 4;
-		printf("real |%ld| |%d|\n", write(fd2, s5, len2), errno);
+		// printf("real |%ld| |%d|\n", write(fd2, s5, len2), errno);
 		printf("own  |%ld| |%d|\n", ft_write(fd, s4, len), errno);
 	}
 	{
