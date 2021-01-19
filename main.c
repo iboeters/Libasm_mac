6d017975a4903c453110d2f1727d26bf42e5e8cc
Iris <iboeters@LAPTOP-MGJ0DHHN.localdomain>
2021-01-19 12:12:05 +0100

    changed flags

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: iboeters <iboeters@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/01/15 13:37:27 by iboeters      #+#    #+#                 */
-/*   Updated: 2021/01/19 12:11:10 by iboeters      ########   odam.nl         */
+/*   Updated: 2021/01/19 12:11:53 by iboeters      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,7 +48,7 @@ int		main(void)
 		int len = 5;
 
 		printf("\ntest ft_write\n");
-		fd = open("file.txt", O_RDONLY);
+		fd = open("file.txt", O_WRONLY);
 		// printf("real |%ld| |%d|\n", write(fd, s4, len), errno);
 		printf("own  |%ld| ||\n", ft_write(fd, s4, len));
 	}
